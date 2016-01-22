c2bbf1fe9741f0899833b60de410393413232f0b
栋栋晓 <2243980491@qq.com>
2016-01-23 03:36:55 +0800

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,15 @@
 class User < ActiveRecord::Base
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent: :destroy  
+
+
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, throuth: :passive_relationships, source: :follower
 
 	attr_accessor :remember_token, :activation_token, :reset_token
 	before_save :downcase_email
@@ -77,6 +87,42 @@ class User < ActiveRecord::Base
     def password_reset_expired?
     	reset_sent_at < 2.hours.ago
     end
+
+
+
+    # 关注另一个用户
+    def follow(other_user)
+      active_relationships.create(follower_id: other_user.id)
+    end
+
+    # 取消关注另一个用户
+    def unfollow(other_user)
+      active_relationships.find_by(followed_id: other_user.id).destroy
+    end
+
+    # 如果当前用户关注了指定的用户，返回 true
+    def following?(other_user)
+      following.include?(other_user)
+    end
+
+
+    # 返回用户的动态流
+    def feed
+      following_ids = "SELECT followed_id FROM relationships
+                     WHERE  follower_id = :user_id"
+      Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
+    end
+
+    # 关注另一个用户
+    def follow(other_user)
+      active_relationships.create(followed_id: other_user.id)
+    end
+
+
+
+
+
+
 
 
 
